ab85f5cb17ca60b12824da42e25a581143f97967
r0mrsom <173564029+r0mrsom@users.noreply.github.com>
2025-09-22 20:26:39 +0800

    Modify script.js and index.html (Website can now set random grid size)

--- script.js
+++ script.js
@@ -1,7 +1,82 @@
-const box = document.querySelectorAll(".column");
-const container = document.querySelector(".container");
+const box = document.querySelectorAll(".row");
+const inputContainer = document.querySelector(".gridInput");
+const button = document.querySelector("button");
+const website = document.querySelector("body")
+
+button.textContent = "Clear";
+document.querySelector(".gridInput label").remove();
+document.querySelector(".gridInput input").remove();
 
 box.forEach((item) => item.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "pink";
+    e.target.style.backgroundColor = "#69432D";
     }
 ))
+
+button.addEventListener("click", (e) => {
+    if (e.target.innerText == "Clear") {
+        document.querySelector(".container").remove();
+        inputContainer.innerHTML = '<label for="gridSize" type="number" min="16" max="100"><b>Input:</b> <i>n</i> (returns an n×n grid)</label>'
+        inputContainer.innerHTML += '<input class="gridSize" type="number" max="100" min="16">'
+        button.textContent = "Submit";
+    }
+    else if (e.target.innerText == "Submit") {
+        const input = document.querySelector(".gridInput input")
+        if (input.value >= 16 && input.value <= 100) {
+            testing();
+        }
+        else {
+            const label = inputContainer.querySelector("label");
+            label.innerHTML = "<b>Invalid</b> <i>(Should be in-between 16 and 100)</i><br> <b>Input:</b> <i>n</i> (returns an n×n grid)"
+
+        }
+    }
+})
+
+function gridCreator() {
+    const newContainer = document.createElement("div");
+    const input = document.querySelector(".gridInput input")
+    newContainer.className = "container";
+
+    const newRow = document.createElement("div");
+    newRow.className = "row";
+    
+    const newColumn = document.createElement("div");
+    newColumn.className = "column";
+
+    console.log(input.value);
+
+    website.appendChild(newContainer);
+
+    //create 1st row element
+    newContainer.appendChild(newRow);
+    //create columns
+    for (let i = 0; i < input.value; i++) {
+        newRow.appendChild(newColumn);
+        newRow.innerHTML += "";
+    }
+
+    //attach remaining rows
+    for (let j = 1; j < input.value; j++) {
+        newContainer.innerHTML += "";
+        newContainer.appendChild(newRow);
+    }
+
+}
+
+function testing() {
+
+        gridCreator();
+        const box = document.querySelectorAll(".row");
+        box.forEach((item) => item.addEventListener("mouseover", (e) => {
+            e.target.style.backgroundColor = "#69432D";
+            }
+        ))
+        document.querySelector(".gridInput label").remove();
+        document.querySelector(".gridInput input").remove();
+        button.textContent = "Clear";
+}
+
+
+
+
+
